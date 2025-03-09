7bd0cfa4234622728568a391e7a29a2693bee451
Alejandro_Vieira <alejandrovieira1912@gmail.com>
2025-03-09 19:07:04 +0000

    refactoring in DetailPokemonFragment
    interface for buttonClicked to change fragments in MainActivity.
    
    implementation of interface in MainActivity and back navigation code implementation. TO SEE.

--- app/src/main/java/com/pokemon_app/MainActivity.java
+++ app/src/main/java/com/pokemon_app/MainActivity.java
@@ -2,25 +2,44 @@ package com.pokemon_app;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+import com.pokemon_app.ui.view.DetailPokemonFragment;
+import com.pokemon_app.ui.view.ListPokemonFragment;
+
+public class MainActivity extends AppCompatActivity implements ListPokemonFragment.OnButtonClicked {
+
+    FragmentManager fragmentManager;
+    View detailPokemonFragment;
+    View listPokemonFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        fragmentManager = getSupportFragmentManager();
+        detailPokemonFragment = findViewById(R.id.detailPokemonFragment);
+        listPokemonFragment = findViewById(R.id.listPokemonFragment);
+
+       replaceFragment(R.id.listPokemonFragment, new ListPokemonFragment(), false);
     }
 
     @Override
@@ -28,5 +47,48 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         getSupportActionBar().setTitle("Pokemon Search List");
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onButtonClickedToChangeFragment() {
+        // Atualizando a visibilidade
+        showFragment(detailPokemonFragment, listPokemonFragment);
+
+        replaceFragment(R.id.detailPokemonFragment, new DetailPokemonFragment(), true);
+
+        getSupportActionBar().setTitle("Pokemon Detail");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void replaceFragment(int containerViewId, Fragment fragment, boolean addToBackStack) {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        transaction.replace(containerViewId, fragment);
+
+        if (addToBackStack) {transaction.addToBackStack(null);}
+
+        // Executa a transação
+        transaction.commit();
+    }
+
+
+    // Função para gerenciar a visibilidade dos fragmentos
+    private void showFragment(View fragmentToShow, View fragmentToHide) {
+        fragmentToShow.setVisibility(View.VISIBLE);
+        fragmentToHide.setVisibility(View.GONE);
+    }
+
+    // Tratando o clique no botão de voltar (ActionBar)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            fragmentManager.popBackStack();
+
+            // Atualizando a visibilidade
+            showFragment(listPokemonFragment, detailPokemonFragment);
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/pokemon_app/ui/view/ListPokemonFragment.java
+++ app/src/main/java/com/pokemon_app/ui/view/ListPokemonFragment.java
@@ -1,21 +1,55 @@
 package com.pokemon_app.ui.view;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.pokemon_app.R;
 
 
 public class ListPokemonFragment extends Fragment {
+
+    Button changeFragmentBtn;
+    OnButtonClicked activity;
+
+   public interface OnButtonClicked {
+        void onButtonClickedToChangeFragment();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list_pokemon, container, false);
+
+        LinearLayout searchBarContainer = view.findViewById(R.id.searchBarContainer);
+
+        changeFragmentBtn = view.findViewById(R.id.button);
+
+
+        changeFragmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.onButtonClickedToChangeFragment();
+            }
+        });
+
+        inflater.inflate(R.layout.search_bar, searchBarContainer, true);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_pokemon, container, false);
+        return view;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        activity = (OnButtonClicked) context;
     }
 }
