e1c9a01da297ed55f8b34ed0c7c8ffc74177594e
Alejandro_Vieira <alejandrovieira1912@gmail.com>
2025-03-09 20:52:37 +0000

    buttons shape created for style used in into_screen fragment xml.
    added colors.
    
    fragment_pokemon_intro_screen.xml created and implemented.
    
    PokemonIntroductionScreen.java fragment created.
    
    added constants strings.

---
+++ app/src/main/java/com/pokemon_app/ui/view/PokemonIntroductionScreen.java
@@ -0,0 +1,22 @@
+package com.pokemon_app.ui.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pokemon_app.R;
+
+
+public class PokemonIntroductionScreen extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pokemon_intro_screen, container, false);
+    }
+}
