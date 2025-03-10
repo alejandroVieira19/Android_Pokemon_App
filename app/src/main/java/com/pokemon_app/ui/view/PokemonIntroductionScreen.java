115472c6f6689d73876e816bb5c1dc2de65e664f
Alejandro_Vieira <alejandrovieira1912@gmail.com>
2025-03-10 13:31:03 +0000

    PokemonIntroductionScreen implemented using dataBinding and fragmentHelper, fragTags and actionbarHelper.
    xml used layout to associate the fragment data to the buttons.

--- app/src/main/java/com/pokemon_app/ui/view/PokemonIntroductionScreen.java
+++ app/src/main/java/com/pokemon_app/ui/view/PokemonIntroductionScreen.java
@@ -1,22 +1,74 @@
 package com.pokemon_app.ui.view;
 
+
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.pokemon_app.R;
+import com.pokemon_app.databinding.FragmentPokemonIntroScreenBinding;
+import com.pokemon_app.utils.ActionBarHelper;
+import com.pokemon_app.utils.FragmentHelper;
+import com.pokemon_app.utils.FragmentsTags;
 
 
+/**
+ * Fragmento que exibe a tela de introdução do Pokémon.
+ * Ele contém dois botões: "Get Started" e "My Favorites".
+ * Ao clicar em cada um, ele substitui o fragmento atual com novos fragmentos.
+ */
 public class PokemonIntroductionScreen extends Fragment {
+    private FragmentPokemonIntroScreenBinding binding;
+    FragmentHelper fragmentHelper;
 
+    ActionBarHelper actionBarHelper;
+
+    /**
+     * Cria e retorna a vista do fragmento, configurando os botões e o FragmentHelper.
+     *
+     * @param inflater O LayoutInflater para inflar a interface do fragmento
+     * @param container O container onde o fragmento será inserido
+     * @param savedInstanceState O estado salvo, se houver
+     * @return A vista inflada do fragmento
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pokemon_intro_screen, container, false);
+
+        binding = FragmentPokemonIntroScreenBinding.inflate(inflater, container, false);
+
+        // serve para vincular a lógica do Fragmento com o layout de maneira dinâmica,
+        binding.setIntroScreenFrag(this);
+
+        // buscar a actividade para usar o actionBarHelper
+        actionBarHelper = new ActionBarHelper((AppCompatActivity) getActivity());
+
+        fragmentHelper = new FragmentHelper(getActivity().getSupportFragmentManager());
+
+        return binding.getRoot();
+    }
+
+    /**
+     * Método chamado quando o botão "Get Started" é clicado.
+     * Substitui o fragmento atual pelo fragmento de lista de Pokémon.
+     */
+    public void onGetStartedClick() {
+        boolean isToAddInStack = true;
+        fragmentHelper.replaceFragment(R.id.mainFrag, new ListPokemonFragment(), isToAddInStack, FragmentsTags.TAG_FRAGMENT_LIST);
+        actionBarHelper.changeActionBarTitleAndShowArrowBack("Pokemon List", isToAddInStack);
+    }
+
+    /**
+     * Método chamado quando o botão "My Favorites" é clicado.
+     * Substitui o fragmento atual pelo fragmento de favoritos de Pokémon.
+     */
+    public void onMyFavoritesClick() {
+        boolean isToAddInStack = true;
+        fragmentHelper.replaceFragment(R.id.mainFrag, new MyFavoritesPokemonFragment(), isToAddInStack, FragmentsTags.TAG_FRAGMENTS_FAVORITES);
+        actionBarHelper.changeActionBarTitleAndShowArrowBack("PokeFavorites", isToAddInStack);
     }
 }
