1b6e2cfde5386138778f4d8ee95f7c9d44df163e
Alejandro_Vieira <alejandrovieira1912@gmail.com>
2025-03-10 13:29:57 +0000

    Refactoring in MainActivity and my favorites fragment created.

--- app/src/main/java/com/pokemon_app/MainActivity.java
+++ app/src/main/java/com/pokemon_app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.pokemon_app;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -14,11 +15,15 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.pokemon_app.ui.view.DetailPokemonFragment;
 import com.pokemon_app.ui.view.ListPokemonFragment;
+import com.pokemon_app.ui.view.PokemonIntroductionScreen;
+import com.pokemon_app.utils.FragmentHelper;
 
 public class MainActivity extends AppCompatActivity implements ListPokemonFragment.OnButtonClicked {
 
     FragmentManager fragmentManager;
-    Fragment detailPokemonFragment, listPokemonFragment;
+    Fragment detailPokemonFragment, listPokemonFragment, pokemonIntroScreen;
+
+    FragmentHelper fragmentHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +41,14 @@ public class MainActivity extends AppCompatActivity implements ListPokemonFragme
         });
 
         fragmentManager = getSupportFragmentManager();
+        fragmentHelper = new FragmentHelper(fragmentManager);
+
         listPokemonFragment = new ListPokemonFragment();
         detailPokemonFragment = new DetailPokemonFragment();
+        pokemonIntroScreen = new PokemonIntroductionScreen();
 
-        pokeExplorerSetup();
+
+
     }
 
 
@@ -50,10 +59,7 @@ public class MainActivity extends AppCompatActivity implements ListPokemonFragme
         return super.onCreateOptionsMenu(menu);
     }
 
-    private void pokeExplorerSetup() {
-        // Adicionando o fragmento da lista de pokémons ao início
-        replaceFragment(R.id.main, listPokemonFragment, false);
-    }
+
 
     private void changeActionBarTitle(String newTile, boolean isArrowBackToShow) {
         getSupportActionBar().setTitle(newTile);
@@ -65,47 +71,16 @@ public class MainActivity extends AppCompatActivity implements ListPokemonFragme
 
     @Override
     public void onButtonClickedToChangeFragment() {
-        // Atualizando a visibilidade dos fragmentos com hide e show
-        showFragment(detailPokemonFragment, listPokemonFragment);
-        replaceFragment(R.id.mainFrag, detailPokemonFragment, true);
-        changeActionBarTitle("Pokemon Detail", true);
+        fragmentHelper.replaceFragment(R.id.mainFrag, detailPokemonFragment, true, "DetailScreen");
     }
 
-    private void replaceFragment(int containerViewId, Fragment fragment, boolean addToBackStack) {
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(containerViewId, fragment);
 
-        if (addToBackStack) {
-            transaction.addToBackStack(null); // Adiciona à pilha de volta
-        }
-
-        transaction.commit();
-    }
-
-    // Função para gerenciar a visibilidade dos fragmentos usando hide/show
-    private void showFragment(Fragment fragmentToShow, Fragment fragmentToHide) {
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-
-        // Esconde o fragmento atual
-        transaction.hide(fragmentToHide);
-
-        // Exibe o fragmento desejado
-        transaction.show(fragmentToShow);
-
-        // Commit da transação
-        transaction.commit();
-    }
 
     // Tratando o clique no botão de voltar (ActionBar)
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             fragmentManager.popBackStack(); // Volta para o fragmento anterior
-
-            // Atualizando a visibilidade ao voltar
-            showFragment(listPokemonFragment, detailPokemonFragment);
-
-            changeActionBarTitle("PokeExplorer App", false);
 
             return true;
         }
