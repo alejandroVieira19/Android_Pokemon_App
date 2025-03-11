d960671a0f24a5adcafc9e43667c7e0555664c04
Alejandro_Vieira <alejandrovieira1912@gmail.com>
2025-03-11 10:56:00 +0000

    implementation of some buttons, and binding used in DetailPokemonFragment

--- app/src/main/java/com/pokemon_app/ui/view/DetailPokemonFragment.java
+++ app/src/main/java/com/pokemon_app/ui/view/DetailPokemonFragment.java
@@ -2,22 +2,63 @@ package com.pokemon_app.ui.view;
 
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.pokemon_app.R;
+import com.pokemon_app.databinding.FragmentDetailPokemonBinding;
+import com.pokemon_app.utils.FragmentHelper;
+
+import org.w3c.dom.Text;
 
 
 public class DetailPokemonFragment extends Fragment {
 
+    private FragmentDetailPokemonBinding binding;
+
+    TextView aboutMeBtn;
+
+    TextView movesBtn;
+    FragmentHelper fragmentHelper;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_pokemon, container, false);
+       binding = FragmentDetailPokemonBinding.inflate(inflater, container, false);
+
+       // GARANTIR QUE O FRAGMENTO ESTEJA VINCULADO AO LAYOUT!!!!!
+       binding.setDetailPokemonFragment(this);
+
+       Log.d("Fragment", "Created");
+
+       fragmentHelper = new FragmentHelper(getActivity().getSupportFragmentManager());
+
+       aboutMeBtn = binding.textAboutMe;
+
+       movesBtn = binding.movesBtn;
+
+       return binding.getRoot();
     }
+
+
+    public void onMovesClick() {
+        aboutMeBtn.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.card_background_transparent));
+        movesBtn.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.card_background_blur));
+
+        Log.d("Tocou 1", "clicou");
+        Log.d("Tocou2", requireContext().getPackageName());
+    }
+
+    public void onAboutMeClick() {
+        movesBtn.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.card_background_transparent));
+        aboutMeBtn.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.card_background_blur));
+    }
+
 }
