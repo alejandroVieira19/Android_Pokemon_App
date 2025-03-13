9f132954fdb85cff6b4c1221d48ba5d26039549f
Alejandro_Vieira <alejandrovieira1912@gmail.com>
2025-03-13 10:15:23 +0000

    PokeCardAdapter created and implemented using binding.
    ListPokemonFragment using the PokeCardAdapter and PokemonViewModel
    
    fragment_list_pokemon.xml adjustments

---
+++ app/src/main/java/com/pokemon_app/presentation/adapter/PokeCardAdapter.java
@@ -0,0 +1,75 @@
+package com.pokemon_app.presentation.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import com.pokemon_app.databinding.RowPokemonCardBinding;
+import com.pokemon_app.domain.model.Pokemon;
+import com.pokemon_app.utils.PokemonUtils;
+
+import java.util.ArrayList;
+
+public class PokeCardAdapter extends RecyclerView.Adapter<PokeCardAdapter.ViewHolder> {
+
+    private ArrayList<Pokemon> pokemonsList;
+
+    public PokeCardAdapter(Context context, ArrayList<Pokemon> list) {
+        pokemonsList = list;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final RowPokemonCardBinding binding;
+        ImageView ivPokemonImage;
+        CardView pokeCardView;
+        TextView tvPokemonName;
+        public ViewHolder(RowPokemonCardBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+            ivPokemonImage = binding.pokemonImage;
+            tvPokemonName = binding.pokemonName;
+            pokeCardView = binding.pokeCardView;
+        }
+
+        public void bind(Pokemon pokemon) {
+
+            binding.setPokemon(pokemon); // Liga o Pokemon à variável no layout XML
+
+            binding.executePendingBindings();
+
+            tvPokemonName.setText(pokemon.getPokemonName());
+
+            pokeCardView.setCardBackgroundColor(
+                    Color.parseColor(PokemonUtils.getColorForPokemonByType(pokemon.getPokemonType().get(0))));
+
+            Glide.with(ivPokemonImage)
+                    .load(pokemon.getPokemonImageUrlCard())
+                    .centerCrop()
+                    .into(binding.pokemonImage);
+        }
+    }
+
+        @NonNull
+        @Override
+    public PokeCardAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RowPokemonCardBinding binding = RowPokemonCardBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PokeCardAdapter.ViewHolder holder, int position) {
+        Pokemon pokemon = pokemonsList.get(position);
+        holder.bind(pokemon);
+    }
+
+    @Override
+    public int getItemCount() {
+        return pokemonsList.size();
+    }
+}
--- app/src/main/java/com/pokemon_app/presentation/ui/view/ListPokemonFragment.java
+++ app/src/main/java/com/pokemon_app/presentation/ui/view/ListPokemonFragment.java
@@ -4,19 +4,38 @@ import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.pokemon_app.R;
+import com.pokemon_app.domain.model.Pokemon;
+import com.pokemon_app.presentation.adapter.PokeCardAdapter;
+import com.pokemon_app.presentation.viewmodel.PokemonViewModel;
+
+import java.util.ArrayList;
 
 
 public class ListPokemonFragment extends Fragment {
 
+    PokeCardAdapter pokeCardAdapter;
+    private PokemonViewModel pokemonViewModel;
+
+    RecyclerView recyclerView;
+
+    RecyclerView.LayoutManager layoutManager;
     Button changeFragmentBtn;
     OnButtonClicked activity;
 
@@ -31,20 +50,48 @@ public class ListPokemonFragment extends Fragment {
 
         LinearLayout searchBarContainer = view.findViewById(R.id.searchBarContainer);
 
-        changeFragmentBtn = view.findViewById(R.id.button);
+        inflater.inflate(R.layout.search_bar, searchBarContainer, true);
+
+        recyclerView = view.findViewById(R.id.pokemonRecyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        // Set GridLayoutManager with 2 columns
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
+        layoutManager = new LinearLayoutManager(this.getActivity());
+        recyclerView.setLayoutManager(gridLayoutManager);
 
 
-        changeFragmentBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                activity.onButtonClickedToChangeFragment();
+        // Initialize ViewModel
+        pokemonViewModel = new ViewModelProvider(requireActivity()).get(PokemonViewModel.class);
+
+
+        pokemonViewModel.getPokemonsData().observe(getViewLifecycleOwner(), pokemonData -> {
+
+            if (pokemonData != null) {
+
+                if (pokemonData.isLoading()) {
+
+                    Log.d("Frag", "Carregando");
+                } else {
+                    if (pokemonData.getPokemons() != null) {
+                        // Set the adapter with the pokemons data
+                        pokeCardAdapter = new PokeCardAdapter(getContext(), (ArrayList<Pokemon>) pokemonData.getPokemons());
+                        recyclerView.setAdapter(pokeCardAdapter);
+                    } else if (pokemonData.getError() != null) {
+                        // Show error if exists
+                        Toast.makeText(getContext(), pokemonData.getError(), Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
 
-        inflater.inflate(R.layout.search_bar, searchBarContainer, true);
-        // Inflate the layout for this fragment
+        pokemonViewModel.fetchAllPokemons(10); // Fetch 151 pokemons
+
+
         return view;
     }
+
+
 
     @Override
     public void onAttach(@NonNull Context context) {
