6ad66d0b69032a01bf80fa3bc4f54e06750ca7cd
Alejandro_Vieira <alejandrovieira1912@gmail.com>
2025-03-16 23:04:57 +0000

    fragment_detail_pokemon.xml refactoring in UI presentation, Changing About me and Moves buttons positions.
    Created more tags for DetailPokemonFragment sub fragments.

--- app/src/main/java/com/pokemon_app/utils/FragmentsTags.java
+++ app/src/main/java/com/pokemon_app/utils/FragmentsTags.java
@@ -6,4 +6,7 @@ public class FragmentsTags {
     public static final String TAG_FRAGMENTS_INTRO = "TAG_FRAGMENT_INTRO";
     public static final String TAG_FRAGMENTS_FAVORITES = "TAG_FRAGMENT_FAVORITES";
 
+    public static final String TAG_FRAGMENTS_POKEMON_MOVES = "TAG_FRAGMENTS_POKEMON_MOVES";
+
+    public static final String TAG_FRAGMENTS_POKEMON_ABOUT_ME = "TAG_FRAGMENTS_POKEMON_ABOUT_ME";
 }
